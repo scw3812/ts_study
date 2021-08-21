cc640016c2b4f88ec9c03f0df79a3a3cc79f2387
scw3812 <dlffpraos@naver.com>
2021-08-21 23:49:47 +0900

    tsc

---
+++ dist/example5.js
@@ -0,0 +1,8 @@
+"use strict";
+var Person = /** @class */ (function () {
+    function Person() {
+        this.name = '';
+        this.age = -1;
+    }
+    return Person;
+}());
---
+++ dist/example6.js
@@ -0,0 +1,5 @@
+"use strict";
+var p6 = {
+    name: 'sdf',
+    age: 3
+};
