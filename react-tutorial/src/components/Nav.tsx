e56c8dfb4da4d5493b483f8a7bb991740ec2f5c8
scw3812 <dlffpraos@naver.com>
2021-08-30 16:35:07 +0900

    react router

---
+++ react-tutorial/src/components/Nav.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <>
+      <h1>Nav</h1>
+      <ul className="navList">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
