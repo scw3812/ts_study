bc7f76a8c3e9b4f24e2a7a1c86991bfafe3b52cf
scw3812 <dlffpraos@naver.com>
2021-09-02 23:59:28 +0900

    add button

--- react-tutorial/src/components/Login/LoginMain.tsx
+++ react-tutorial/src/components/Login/LoginMain.tsx
@@ -1,6 +1,6 @@
-import { Link } from 'react-router-dom';
-import Footer from '../Footer';
-import './LoginMain.css'
+import { Link } from "react-router-dom";
+import Footer from "../Footer";
+import "./LoginMain.css";
 
 export default function LoginMain() {
   return (
@@ -8,7 +8,13 @@ export default function LoginMain() {
       <section className="idAndPasswd">
         <div className="id"></div>
         <div className="passwd"></div>
-        <div className="buttonContainer"></div>
+        <div className="buttonContainer">
+          <div className="d-grid gap-2">
+            <button className="btn btn-info" type="button">
+              Button
+            </button>
+          </div>
+        </div>
       </section>
       <section className="wannaJoin">
         <span>혹시 조교도시락이 처음이신가요?</span>
