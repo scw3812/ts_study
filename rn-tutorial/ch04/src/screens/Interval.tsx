f12fadd03114b94ff7cee00133e754c11b46bd54
scw3812 <dlffpraos@naver.com>
2021-09-13 03:24:43 +0900

    useEffect 2

--- rn-tutorial/ch04/src/screens/Interval.tsx
+++ rn-tutorial/ch04/src/screens/Interval.tsx
@@ -1,15 +1,41 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useCallback, useEffect } from 'react'
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
 import { Colors } from 'react-native-paper'
+import { Avatar } from '../components'
 import * as D from '../data'
 
-const people = D.makeArray(2).map(D.createRandomPerson)
+type IdAndAvatar = Pick<D.IPerson, 'id' | 'avatar'>
 
 const title = 'Interval'
 const Interval = () => {
+  const [avatars, setAvatars] = useState<IdAndAvatar[]>([])
+  const [start, setStart] = useState(true)
+  const toggleStart = useCallback(() => setStart(flag => !flag), [])
+  const clearAvatars = useCallback(() => setAvatars(() => []), [])
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (start) {
+        setAvatars(state => [...state, { id: D.randomId(), avatar: D.randomAvatarUri() }])
+      }
+    }, 1000)
+
+    return () => clearInterval(id)
+  }, [start])
+
+  const children = avatars.map(({ id, avatar }) => <Avatar key={id} uri={avatar} size={70} viewStyle={styles.avatar} />)
+
   return (
     <View style={styles.view}>
+      <View style={styles.topBar}>
+        <Text onPress={toggleStart} style={styles.topBarText}>
+          {start ? 'stop' : 'start'}
+        </Text>
+        <Text onPress={clearAvatars} style={styles.topBarText}>
+          clear avatars
+        </Text>
+      </View>
       <Text style={styles.text}>{title}</Text>
+      <ScrollView contentContainerStyle={styles.contentContainerStyle}>{children}</ScrollView>
     </View>
   )
 }
@@ -19,4 +45,14 @@ export default Interval
 const styles = StyleSheet.create({
   view: { flex: 1, padding: 5, backgroundColor: Colors.blue900 },
   text: { fontSize: 20, color: 'white' },
+  avatar: { padding: 5 },
+  topBar: {
+    width: '100%',
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'space-between',
+    backgroundColor: Colors.blue900,
+  },
+  topBarText: { fontSize: 20, color: 'white' },
+  contentContainerStyle: { flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center' },
 })
