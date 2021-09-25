bbec7da3c4d25a93003c198fc9ad4619bea075cf
scw3812 <scw3812@naver.com>
2021-09-26 02:55:53 +0900

    stagger

--- rn-tutorial/ch06/src/screens/PersonStagger.tsx
+++ rn-tutorial/ch06/src/screens/PersonStagger.tsx
@@ -1,20 +1,22 @@
-import React, {
-  useCallback,
-  useState,
-  useRef,
-  useEffect,
-  useMemo,
-} from 'react';
+/* eslint-disable react-native/no-inline-styles */
+import React, { useCallback, useMemo } from 'react';
 import type { FC } from 'react';
-import { View, Text, Alert, Image, Animated, Easing } from 'react-native';
+import { View, Text, Image, Animated } from 'react-native';
 import moment from 'moment-with-locales-es6';
 import { Colors } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as D from '../data';
 import { styles } from './Person.style';
 import { Avatar } from '../components';
+import FontawesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { useAnimatedValues, useToggle, useLayout } from '../hooks';
+import { interpolate } from '../utils';
 
 moment.locale('ko');
+
+const AnimatedIcon = Animated.createAnimatedComponent(FontawesomeIcon);
+const IconSize = 30;
+const delay = 1000;
 
 export type PersonProps = {
   person: D.IPerson;
@@ -22,7 +24,76 @@ export type PersonProps = {
 };
 
 const PersonStagger: FC<PersonProps> = ({ person, deletePressed }) => {
-  const avatarPressed = useCallback(() => Alert.alert('avatar'), []);
+  const [started, toggleStarted] = useToggle();
+  const [layout, setLayout] = useLayout();
+
+  const balls = useMemo(
+    () => [Colors.pink500, Colors.lime500, Colors.lightBlue500],
+    [],
+  );
+  const animValues = useAnimatedValues(balls.length);
+
+  const startAnimations = useMemo(
+    () =>
+      balls
+        .map((_, index) =>
+          Animated.spring(animValues[index], {
+            useNativeDriver: true,
+            toValue: 1,
+          }),
+        )
+        .reverse(),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+  const endAnimations = useMemo(
+    () =>
+      balls.map((_, index) =>
+        Animated.spring(animValues[index], {
+          useNativeDriver: true,
+          toValue: 0,
+        }),
+      ),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [],
+  );
+
+  const avatarPressed = useCallback(() => {
+    Animated.stagger(delay, [...startAnimations, ...endAnimations]).start(
+      toggleStarted,
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [started]);
+
+  const icons = useMemo(
+    () =>
+      balls.map((color, index) => {
+        const numberOfIcons = balls.length;
+        const animValue = animValues[index];
+        const animStyle = {
+          transform: [
+            {
+              translateX: interpolate(animValue, [
+                0,
+                layout.width - numberOfIcons * IconSize,
+              ]),
+            },
+            { rotate: interpolate(animValue, ['0deg', '720deg']) },
+          ],
+        };
+        return (
+          <AnimatedIcon
+            key={color}
+            style={animStyle}
+            name="soccer-ball-o"
+            size={IconSize}
+            color={color}
+          />
+        );
+      }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [layout.width],
+  );
 
   return (
     <View style={styles.view}>
@@ -56,10 +127,10 @@ const PersonStagger: FC<PersonProps> = ({ person, deletePressed }) => {
           {person.comments}
         </Text>
         <Image source={{ uri: person.image }} style={styles.image} />
-        <View style={styles.countsView}>
-          <Icon name="comment" size={24} color={Colors.blue500} />
-          <Icon name="twitter-retweet" size={24} color={Colors.purple500} />
-          <Icon name="heart" size={24} color={Colors.red500} />
+        <View
+          onLayout={setLayout}
+          style={[styles.countsView, { justifyContent: 'flex-start' }]}>
+          {icons}
         </View>
       </View>
     </View>
